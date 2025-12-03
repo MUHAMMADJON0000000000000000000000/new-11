7268c955d37298902226e17aac367e6cae7cf4fd
Muhammadjon coding <86595049+MUHAMMADJON0000000000000000000000@users.noreply.github.com>
2025-12-03 17:50:50 +0500

    hello

---
+++ index.js
@@ -0,0 +1,87 @@
+/// uztoz kanspekt degandi bu uzim yozganman va uzim uhun kanspekt
+// // string
+// let color = "Yellow";
+// let LastName = "Johnson";
+
+// //number
+// let length = 22;
+// let weight = 21;
+
+// //BigInit
+// let x = 12319274178942719378197249817498;
+// let y = BigInt(12319274178942719378197249817498);
+
+// // Boolean
+// let x = true;
+// let x = false;
+
+// //Object
+// const cars = ["saab", "Volvo", "BMW"];
+// // Data object
+// const date = new Date("2025.03.25");
+
+// //unfainid
+// let x;
+// let y;
+
+// //Null;
+// const x = Symbol();
+// const y = Symbole();
+
+// // typeof malumot turini topib beradi
+//misollar
+// const value1 = 42;
+// const value2 = "Hello";
+// const value3 = null;
+// console.log(typeof value1);
+// console.log(typeof value2);
+// console.log(typeof value3);
+
+// const isLoggedIn = true;
+// const hasPremium = false;
+// console.log(hasPremium);
+
+// const amountStr = "1250.32";
+// console.log(amountStr + "Type : number");
+
+// const num = 10;
+// const text = "20";
+// let string_type = num + text;
+// console.log(num + text + ":type string");
+// console.log(typeof(string_type));
+
+// const ageNum = 30;
+// let string_type = ageNum;
+// console.log(ageNum + ":type string");
+
+// console.log(typeof string_type);
+
+// const str1 = "false";
+// const str2 = "";
+// console.log(Boolean(str1));
+// console.log(Boolean(str2));
+
+// const a = 0;
+// const b = false;
+
+// console.log(a, b == true);
+// console.log(true);
+
+// const x = "100";
+// const y = 100;
+// // console.log(x, y + false);
+// const deadlineDays = 3;
+// if (deadlineDays > 0) {
+//   console.log(undefined(deadlineDays) + "Muddatga vaqt bor");
+// } else {
+//   console.log("mudadat tugagan yoki belgilanmagan");
+// }
+
+// const input1 = 3;
+// const input2 = "200";
+
+// console.log(typeof input1 + " 10ga 10 aylanadi");
+// console.log(typeof input2 + " 100ga 10 aylanadi");
+// // 23lf,mb;mdf
+// dfb'dfb
+// d'bm'dfb'd
